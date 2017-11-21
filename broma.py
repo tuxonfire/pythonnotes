41198dbb7467d8d4f6dd2ebd8dda03e5a9806fad
linuxonfire <itzelj@localhost.localdomain>
2017-11-21 17:10:23 -0600

    adding py files

---
+++ broma.py
@@ -0,0 +1,5 @@
+chiste = '''como se llama el hermano mayor de pinocho?
+pinueve'''
+print (chiste)
+chiste2 = 'como se llama lalalalala? \n pinueve'
+print (chiste2)
---
+++ hola.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+# -*- coding: latin-1 -*-
+import os, sys
+nombre = input("¿Como te llamas?")
+print("Hola, ", nombre)
