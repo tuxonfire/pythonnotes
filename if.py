1d01ed342e90c780971e1b968716990f74079611
linuxonfire <itzelj@localhost.localdomain>
2017-11-22 14:28:23 -0600

    Adding changes on LearningPython

--- if.py
+++ if.py
@@ -1,11 +1,14 @@
-ahorros = 100
-if ahorros > 299:
+ahorros = 50
+#ahorros = input("¿Cuánto tienes ahorrado?")
+#print("Tienes ahorrado , ", ahorros)
+if ahorros > 300:
     print('comprar xbox')
-else:
-    print('ahorrar mas')
-
-if ahorros ==100:
+elif ahorros > 100:
+    print('tienes ahorrado %s , Ya casi' %ahorros )
+elif ahorros > 200:
+    print('Ya puedes comprar el xbox')
+elif ahorros == 100:
     print("solo tenemos %s" %ahorros )
-    print('Nos faltan 199 para el xbox')
+    print("sigue ahorrando más para el xbox")
 else:
     print('Tenemos otra cantidad de dinero')
