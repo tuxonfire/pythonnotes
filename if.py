aee279f0dde241460e342fae4a44909557523e87
linuxonfire <cosmonauta@Itzels-MacBook-Pro.local>
2017-11-21 22:22:26 -0600

    Sentence IF

---
+++ if.py
@@ -0,0 +1,11 @@
+ahorros = 100
+if ahorros > 299:
+    print('comprar xbox')
+else:
+    print('ahorrar mas')
+
+if ahorros ==100:
+    print("solo tenemos %s" %ahorros )
+    print('Nos faltan 199 para el xbox')
+else:
+    print('Tenemos otra cantidad de dinero')
