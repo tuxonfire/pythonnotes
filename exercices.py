d484ca9511919a09c978d68d307e87777f9d7625
Itzel Jaimes Campos <felixitzel.jaimes.campos@bbva.com>
2017-11-27 17:47:37 -0600

    adding some stuff

---
+++ exercices.py
@@ -0,0 +1,5 @@
+def max(x,y):
+    if x > y:
+        print ('x es mayor que y')
+	else:
+        print ('y es mayor que x')
--- files.py
+++ files.py
@@ -1,7 +1,7 @@
 miPrimerArchivo = open("lista.txt", "w")
 miPrimerArchivo.write("peras\n")
 miPrimerArchivo.write("manzanas\n")
-miPrimerArchivo.write("Bananas")
+miPrimerArchivo.write("Bananas\n")
 miPrimerArchivo.close()
 
 print("**Leyendo el archivo: \n")
@@ -17,7 +17,34 @@ miArchivo.close()
 
 print("\n Validando contenido")
 miArchivo = open("lista.txt")
-for linea in miArchivo:
-   if(linea.find("manzanas")>-1):
-      print("Me gustan las ", linea)
+for lista in miArchivo:
+   if(lista.find("manzanas")>-1):
+      print("Me gustan las ", lista)
+   else:
+     print(lista)
 miArchivo.close()
+
+print("\nUsando Readline\n")
+miArchivo = open("lista.txt")
+print(miArchivo.readline())
+print(miArchivo.readline())
+print(miArchivo.readline())
+miArchivo.close()
+
+print("\nUsando ReadlineS\n")
+miArchivo = open("lista.txt")
+print(miArchivo.readlines())
+miArchivo.close()   
+
+print("\nUsando Read\n")
+miArchivo = open("lista.txt")
+print(miArchivo.read(7))
+miArchivo.close()
+
+#WITH
+
+print("\nUsando With: \n")
+with open("lista.txt") as miArchivo:
+    print(miArchivo.read(7))
+
+
