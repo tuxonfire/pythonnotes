13749d5000e805822c5667a468df9292fed420a0
linuxonfire <cosmonauta@Itzels-MacBook-Pro.local>
2017-11-26 22:45:50 -0600

    adding listas

--- files.py
+++ files.py
@@ -1,9 +1,23 @@
 miPrimerArchivo = open("lista.txt", "w")
 miPrimerArchivo.write("peras\n")
-miPrimerArchivo.write("manzanas")
+miPrimerArchivo.write("manzanas\n")
+miPrimerArchivo.write("Bananas")
 miPrimerArchivo.close()
 
 print("**Leyendo el archivo: \n")
 miArchivo = open("lista.txt")
 print(miArchivo.read())
+miArchivo.close()
+
+print("\n**Linea por linea.\n")
+miArchivo = open("lista.txt")
+for linea in miArchivo:
+    print(linea)
+miArchivo.close()
+
+print("\n Validando contenido")
+miArchivo = open("lista.txt")
+for linea in miArchivo:
+   if(linea.find("manzanas")>-1):
+      print("Me gustan las ", linea)
 miArchivo.close()
